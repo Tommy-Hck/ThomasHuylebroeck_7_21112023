4e393adcd40d202e1d06839b8878526661b680f2
Tommy-Hck <huylebroeckt@gmail.com>
2023-11-27 18:17:38 +0100

    gestion des tailles d'écran sur home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,26 @@ import banner from '../assets/bannière.png'
 import '../styles/Home.scss'
 
 export default function Home() {
-    const [width, setWidth] = useState(window.innerWidth)
+    const [width, setWidth] = useState(window.innerWidth) //hook changement d'état
+
+    const handleResize = () => {
+        setWidth(window.innerWidth)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    console.log('width', width)
   return (
     <main>
         <div className='banner'>
             <img src={banner} alt='bannière'/>
-            <h1 className='banner-title'>Chez vous, partout et ailleurs</h1>
+            {width > 425 ? (     //rendu conditionnel if/else
+                <h1 className='banner-title'>Chez vous, partout et ailleurs</h1>
+            ) : (
+                <h1 className='banner-title'>Chez vous,<br></br> partout et ailleurs</h1>
+            )}
         </div>
     </main>
   )
