bc7a67215c03e259a1b257947b50a4143f257942
Tommy-Hck <huylebroeckt@gmail.com>
2023-11-27 18:04:39 +0100

    ajout de la bannière et du style sur Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import banner from '../assets/bannière.png'
+import '../styles/Home.scss'
 
 export default function Home() {
+    const [width, setWidth] = useState(window.innerWidth)
   return (
-    <div>Home</div>
+    <main>
+        <div className='banner'>
+            <img src={banner} alt='bannière'/>
+            <h1 className='banner-title'>Chez vous, partout et ailleurs</h1>
+        </div>
+    </main>
   )
 }
