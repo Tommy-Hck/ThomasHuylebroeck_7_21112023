aff5742077565b4f11d7a34c4f93a7266ec9525f
Tommy-Hck <huylebroeckt@gmail.com>
2023-11-27 16:39:18 +0100

    création du footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../../public/assets/logoFooter.png'
+
+export default function Footer() {
+  return (
+    <footer>
+        <img src={logo}></img>
+        <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
