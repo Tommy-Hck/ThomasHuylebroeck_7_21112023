f9a17997a16894519685cfa1d7d09dacd077ce84
Tommy-Hck <huylebroeckt@gmail.com>
2023-11-27 22:33:05 +0100

    ajout d'un bouton voir plus voir moins pour les logements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from 'react'
 import banner from '../assets/bannière.png'
 import '../styles/Home.scss'
 import homeData from '../assets/logements.json'
+import arrow from '../assets/flècheBas.png'
 
 export default function Home() {
     const [width, setWidth] = useState(window.innerWidth) //hook changement d'état
+    const [isShow, setIsShow] = useState(false)
 
     const handleResize = () => {
         setWidth(window.innerWidth)
@@ -15,6 +17,7 @@ export default function Home() {
         return () => window.removeEventListener('resize', handleResize)
     }, [])
     console.log('width', width)
+
   return (
     <main>
         <div className='banner'>
@@ -26,7 +29,9 @@ export default function Home() {
             )}
         </div>
         <section className='home-section'>
-            <div className='container'>
+
+            {isShow === false ? (
+                <div className='container'>
                 {homeData
                 .slice(0, 6) //afficher les image d'index 0 à 6
                 .map((home, index) => {
@@ -39,7 +44,27 @@ export default function Home() {
                         </div>
                     )
                 })}
+                <span className='btn btn-more'onClick={() => setIsShow(true)}>voir plus</span>
+                
             </div>
+            ):(
+                <div className='container'>
+                {homeData
+                .map((home, index) => {
+                    return (
+                        <div className='home-card' key={home.id}>
+                            <a href={`/logements/${home.id}`}>
+                        <img src={home.cover} alt={home.title} />
+                        <p className='home-title'>{home.title}</p>
+                        </a>
+                        </div>
+                    )
+                })}
+                <span className='btn btn-less' onClick={() => setIsShow(false)}>voir moins</span>
+            </div>
+            )}
+        
+            
 
         </section>
     </main>
