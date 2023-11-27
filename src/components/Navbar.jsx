59e160b4ed7dcb57596d9c84bd61f8a37d37750b
Tommy-Hck <huylebroeckt@gmail.com>
2023-11-27 16:35:25 +0100

    installation du projet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import logo from '../../public/assets/logo.svg'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+    <div className='nav-wrapper'>
+        <img src={logo} alt="logo" className="brand-logo"/>
+        <div className='nav-link'>
+            <ul>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/about'>About</a>
+                </li>
+
+                
+            </ul>
+        </div>
+    </div>
+    </nav>
   )
 }
